2591c4e232c0db23c302002d0b96a2a65f8842e5
Uxugin <git@uxugin.com>
2024-06-17 17:35:05 -0600

    it works like windows now

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use gtk4::{cairo, glib, Application, ApplicationWindow, DrawingArea, GestureClic
 use cairo::Context;
 use glib::clone;
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 struct Thing {
     pub red: f64,
@@ -45,18 +46,18 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 fn build_ui(app: &Application) {
-    let things = [
+    let things = Rc::new(RefCell::new(VecDeque::from([
         Rc::new(RefCell::new(Thing::new(1.0, 0.0, 0.0, 100.0, 100.0, 200.0, 100.0))),
         Rc::new(RefCell::new(Thing::new(0.0, 0.0, 1.0, 100.0, 100.0, 000.0, 100.0))),
-    ];
+    ])));
     let drawing_area = Rc::new(DrawingArea::builder()
         .content_width(AREA_WIDTH)
         .content_height(AREA_HEIGHT)
         .build());
     drawing_area.set_draw_func(clone!(@strong drawing_area, @strong things => move |_drawing_area: &DrawingArea, context: &Context, _width: i32, _height: i32| {
-        let mut my_things = things.clone();
+        let mut my_things = Vec::from(things.clone().borrow().clone());
         my_things.reverse();
-        for thing in &my_things {
+        for thing in my_things {
             context.set_source_rgb(thing.borrow().red, thing.borrow().green, thing.borrow().blue);
             context.rectangle(thing.borrow().draw_x, thing.borrow().draw_y, thing.borrow().width, thing.borrow().height);
             context.fill().unwrap();
@@ -67,7 +68,8 @@ fn build_ui(app: &Application) {
     drawing_area.add_controller(drag.clone());
     drawing_area.add_controller(click.clone());
     drag.connect_drag_end(clone!(@strong drawing_area, @strong things => move |_gesture: &GestureDrag, x: f64, y: f64| {
-        for thing in &things {
+        let mut export = None;
+        for (i, thing) in things.clone().borrow().clone().into_iter().enumerate() {
             if thing.borrow().dragging {
                 let draw_x = thing.borrow().start_x + thing.borrow().relative_x + x;
                 let draw_y = thing.borrow().start_y + thing.borrow().relative_y + y;
@@ -86,12 +88,21 @@ fn build_ui(app: &Application) {
                     thing.borrow_mut().draw_y = max_y;
                 }
                 drawing_area.queue_draw();
+                export = Some((i, thing));
                 break;
             }
+        }
+        match export {
+            Some((i, thing)) => {
+                things.borrow_mut().remove(i);
+                things.borrow_mut().push_front(thing);
+            }
+            None => {}
         }
     }));
     drag.connect_drag_update(clone!(@strong drawing_area, @strong things => move |_gesture: &GestureDrag, x: f64, y: f64| {
-        for thing in &things {
+        let mut export = None;
+        for (i, thing) in things.clone().borrow().clone().into_iter().enumerate() {
             if thing.borrow().dragging {
                 let draw_x = thing.borrow().start_x + thing.borrow().relative_x + x;
                 let draw_y = thing.borrow().start_y + thing.borrow().relative_y + y;
@@ -110,12 +121,20 @@ fn build_ui(app: &Application) {
                     thing.borrow_mut().draw_y = max_y;
                 }
                 drawing_area.queue_draw();
+                export = Some((i, thing));
                 break;
             }
         }
+        match export {
+            Some((i, thing)) => {
+                things.borrow_mut().remove(i);
+                things.borrow_mut().push_front(thing);
+            }
+            None => {}
+        }
     }));
     click.connect_pressed(clone!(@strong drawing_area, @strong things => move |_gesture: &GestureClick, _click_count: i32, click_x: f64, click_y: f64| {
-        for thing in &things {
+        for thing in things.clone().borrow().clone() {
             thing.borrow_mut().dragging = thing.borrow().draw_x <= click_x && click_x <= thing.borrow().draw_x + 100.0 && thing.borrow().draw_y <= click_y && click_y <= thing.borrow().draw_y + 100.0;
             if thing.borrow().dragging {
                 thing.borrow_mut().start_x = click_x;
