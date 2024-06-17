ffd47e33f03d7d96934516d6fee125839fa86d51
Uxugin <git@uxugin.com>
2024-06-17 07:45:57 -0600

    square thing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use gtk4::prelude::*;
-use gtk4::{glib, Application, ApplicationWindow, DrawingArea};
+use gtk4::{cairo, glib, Application, ApplicationWindow, DrawingArea};
+use cairo::Context;
 const APP_ID: &str = "com.uxugin.gtk-cairo-test";
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
@@ -8,13 +9,25 @@ fn main() -> glib::ExitCode {
 }
 fn build_ui(app: &Application) {
     let drawing_area = DrawingArea::builder()
-        .content_width(256*3)
-        .content_height(256*2)
+        .content_width(300)
+        .content_height(300)
         .build();
+    drawing_area.set_draw_func(draw_func);
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
         .child(&drawing_area)
         .build();
     window.present();
+}
+fn draw_func(_drawing_area: &DrawingArea, context: &Context, _width: i32, _height: i32) {
+    context.set_source_rgb(1.0, 1.0, 1.0);
+    context.paint().unwrap();
+    context.set_source_rgb(0.0, 0.0, 0.0);
+    context.line_to(100.0, 100.0);
+    context.line_to(200.0, 100.0);
+    context.line_to(200.0, 200.0);
+    context.line_to(100.0, 200.0);
+    context.line_to(100.0, 100.0);
+    context.stroke().unwrap();
 }
