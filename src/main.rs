c5c67c6021b553ea34e18e07e3e78ac6e6fa503e
Uxugin <git@uxugin.com>
2024-06-17 06:04:17 -0600

    hello world

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,6 @@ fn main() -> glib::ExitCode {
 fn build_ui(app: &Application) {
     let button = Button::builder()
         .label("Press me!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
         .build();
     button.connect_clicked(|button| {
         button.set_label("Hello World!");
