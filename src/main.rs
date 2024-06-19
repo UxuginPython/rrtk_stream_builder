7ef584649205145f68bf99b1021f73be083b7fc0
Uxugin <git@uxugin.com>
2024-06-19 09:42:18 -0600

    drag stuff

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,75 @@ use gtk4::prelude::*;
 use gtk4::{Application, ApplicationWindow, cairo, DrawingArea, GestureDrag, glib};
 use cairo::Context;
 use glib::clone;
+enum DragInfo {
+    Idle,
+    Move {
+        node: usize,
+        start_x: f64,
+        start_y: f64,
+        relative_x: f64,
+        relative_y: f64,
+    },
+    Connect,
+}
+#[derive(Debug)]
+enum LocalTerminal {
+    Left(u8),
+    Right(u8),
+}
+#[derive(Debug)]
+struct GlobalTerminal {
+    pub node: usize,
+    pub terminal: LocalTerminal,
+}
+#[derive(Debug)]
+enum Clicked {
+    Nothing,
+    Body,
+    Terminal(LocalTerminal),
+}
+#[derive(Debug)]
+struct Node {
+    x: f64,
+    y: f64,
+}
+impl Node {
+    fn new(x: f64, y: f64) -> Self {
+        Self {
+            x: x,
+            y: y,
+        }
+    }
+    fn draw(&self, context: &Context) -> Result<(), cairo::Error> {
+        context.set_source_rgb(0.5, 0.5, 0.5);
+        context.rectangle(self.x, self.y, 50.0, 30.0);
+        context.fill()?;
+        context.set_source_rgb(0.0, 0.0, 0.0);
+        context.rectangle(self.x + 10.0, self.y + 10.0, 10.0, 10.0);
+        context.rectangle(self.x + 30.0, self.y + 10.0, 10.0, 10.0);
+        context.fill()?;
+        Ok(())
+    }
+    fn get_clicked(&self, click_x: f64, click_y: f64) -> Clicked {
+        if self.x + 10.0 <= click_x
+            && click_x <= self.x + 20.0
+            && self.y + 10.0 <= click_y
+            && click_y <= self.x + 20.0 {
+            return Clicked::Terminal(LocalTerminal::Left(0));
+        } else if self.x + 30.0 <= click_x
+            && click_x <= self.x + 40.0
+            && self.y + 10.0 <= click_y
+            && click_y <= self.x + 20.0 {
+            return Clicked::Terminal(LocalTerminal::Right(0));
+        } else if self.x <= click_x
+            && click_x <= self.x + 50.0
+            && self.y <= click_y
+            && click_y <= self.y + 30.0 {
+            return Clicked::Body;
+        }
+        Clicked::Nothing
+    }
+}
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
     app.connect_activate(build_ui);
