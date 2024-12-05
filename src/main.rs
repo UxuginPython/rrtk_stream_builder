61288f1d468a85c3972c3c3732a5cfb4745ca3a8
Uxugin <git@uxugin.com>
2024-12-05 03:06:15 -0700

    kind of line rendering

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ const APP_ID: &str = "com.uxugin.rrtk_stream_builder";
 struct Node {
     type_name: String,
     inputs: Vec<Option<Rc<RefCell<Node>>>>,
+    x: f64,
+    y: f64,
     var_name: Option<String>,
     // |my_var_name: String, input_var_names: Vec<String>| {line_of_code}
     generate_func: Box<dyn Fn(String, Vec<String>) -> String>,
@@ -22,9 +24,14 @@ impl Node {
         Self {
             type_name: type_name,
             inputs: vec![None; input_count],
+            x: 0.0,
+            y: 0.0,
             var_name: None,
             generate_func: generate_func as Box<dyn Fn(String, Vec<String>) -> String>,
         }
+    }
+    fn get_output_coords(&self) -> (f64, f64) {
+        (self.x + 85.0, self.y + 15.0)
     }
     fn new_none_getter() -> Self {
         Self::new(
@@ -57,6 +64,12 @@ fn max_partial_ord<T: PartialOrd>(x: T, y: T) -> T {
 }
 impl Draggable for Node {
     fn draw(&self, context: &Context, x: f64, y: f64) -> Result<(), Error> {
+        unsafe {
+            //self is a reference
+            let ptr = self as *const Node as *mut Node;
+            (*ptr).x = x;
+            (*ptr).y = y;
+        }
         context.set_source_rgb(0.5, 0.5, 0.5);
         context.rectangle(
             x,
@@ -72,6 +85,17 @@ impl Draggable for Node {
             context.rectangle(x + 10.0, terminal_y, 10.0, 10.0);
         }
         context.fill()?;
+        for (i, maybe_input) in self.inputs.iter().enumerate() {
+            let input = match maybe_input {
+                Some(x) => x.borrow(),
+                None => continue,
+            };
+            let (in_x, in_y) = input.get_output_coords();
+            let (my_x, my_y) = (self.x + 15.0, self.y + 15.0 + 20.0 * i as f64);
+            context.line_to(in_x, in_y);
+            context.line_to(my_x, my_y);
+            context.stroke()?;
+        }
         Ok(())
     }
     fn get_limits(&self) -> (f64, f64, f64, f64) {
@@ -90,10 +114,11 @@ fn main() -> glib::ExitCode {
 }
 fn build_ui(app: &Application) {
     let mut drag_area = DragArea::new(500, 500);
-    let quotient = Box::new(Node::new_quotient_stream());
-    let none = Box::new(Node::new_none_getter());
-    drag_area.push_box(quotient, 100.0, 200.0);
-    drag_area.push_box(none, 100.0, 100.0);
+    let none = Rc::new(RefCell::new(Node::new_none_getter()));
+    let quotient = Rc::new(RefCell::new(Node::new_quotient_stream()));
+    quotient.borrow_mut().inputs[0] = Some(Rc::clone(&none));
+    drag_area.push_rc_ref_cell(none, 100.0, 100.0);
+    drag_area.push_rc_ref_cell(quotient, 100.0, 200.0);
     let window = ApplicationWindow::builder()
         .application(app)
         .child(&drag_area)
