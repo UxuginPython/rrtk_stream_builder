3b9847e3aac18bd359b712d8734e2c2e028465bf
Uxugin <git@uxugin.com>
2024-12-11 17:43:33 -0700

    make it better

--- src/scope.rs
+++ src/scope.rs
@@ -21,26 +21,20 @@ pub mod scope {
         none_getter: NoneGetter,
         streams: Streams,
     }
-    struct ConstantGetter {
-        self_in_scope: bool,
-    }
-    struct NoneGetter {
-        self_in_scope: bool,
-    }
-    struct Streams {
-        self_in_scope: bool,
-        expirer_in_scope: bool,
-        latest_in_scope: bool,
-    }
     impl Crate {
         pub fn string_path(&self, path: path::Crate) -> String {
             let crate_path: String = "rrtk::".into();
             match path {
                 path::Crate::ConstantGetter => self.constant_getter.string_path(crate_path),
                 path::Crate::NoneGetter => self.none_getter.string_path(crate_path),
-                path::Crate::Streams(stream_type) => self.streams.string_path(crate_path, stream_type),
+                path::Crate::Streams(stream_type) => {
+                    self.streams.string_path(crate_path, stream_type)
+                }
             }
         }
+    }
+    struct ConstantGetter {
+        self_in_scope: bool,
     }
     impl ConstantGetter {
         fn string_path(&self, super_path: String) -> String {
@@ -52,6 +46,9 @@ pub mod scope {
             }
         }
     }
+    struct NoneGetter {
+        self_in_scope: bool,
+    }
     impl NoneGetter {
         fn string_path(&self, super_path: String) -> String {
             let name = "NoneGetter";
@@ -62,9 +59,51 @@ pub mod scope {
             }
         }
     }
+    struct Streams {
+        self_in_scope: bool,
+        expirer: streams::Expirer,
+        latest: streams::Latest,
+    }
     impl Streams {
         fn string_path(&self, super_path: String, path: path::Streams) -> String {
-            todo!();
+            let name = "streams::";
+            let name: String = if self.self_in_scope {
+                name.into()
+            } else {
+                super_path + name
+            };
+            match path {
+                path::Streams::Expirer => self.expirer.string_path(name),
+                path::Streams::Latest => self.latest.string_path(name),
+            }
+        }
+    }
+    mod streams {
+        pub struct Expirer {
+            self_in_scope: bool,
+        }
+        impl Expirer {
+            pub fn string_path(&self, super_path: String) -> String {
+                let name = "Expirer";
+                if self.self_in_scope {
+                    name.into()
+                } else {
+                    super_path + name
+                }
+            }
+        }
+        pub struct Latest {
+            self_in_scope: bool,
+        }
+        impl Latest {
+            pub fn string_path(&self, super_path: String) -> String {
+                let name = "Latest";
+                if self.self_in_scope {
+                    name.into()
+                } else {
+                    super_path + name
+                }
+            }
         }
     }
 }
