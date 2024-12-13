a2933fb118a042eef95540cf86504b01c77c8e6a
Uxugin <git@uxugin.com>
2024-12-12 20:28:36 -0700

    better

--- src/path.rs
+++ src/path.rs
@@ -10,4 +10,14 @@ pub enum Crate {
 pub enum Streams {
     Expirer,
     Latest,
+    Control(streams::Control),
+}
+pub mod streams {
+    #[derive(Clone, Copy)]
+    pub enum Control {
+        CommandPID,
+        EWMAStream,
+        MovingAverageStream,
+        PIDControllerStream,
+    }
 }
--- src/scope.rs
+++ src/scope.rs
@@ -1,216 +1,89 @@
 #![allow(unused)]
 use super::*;
+fn reduce(super_path: String, name: &str, in_scope: bool) -> String {
+    if in_scope {
+        name.into()
+    } else {
+        super_path + name
+    }
+}
 pub struct Crate {
-    constant_getter: ConstantGetter,
-    none_getter: NoneGetter,
+    constant_getter: bool,
+    none_getter: bool,
     streams: Streams,
 }
 impl Crate {
     pub fn new() -> Self {
         Self {
-            constant_getter: ConstantGetter::new(),
-            none_getter: NoneGetter::new(),
+            constant_getter: false,
+            none_getter: false,
             streams: Streams::new(),
         }
     }
     pub fn string_path(&self, path: path::Crate) -> String {
         let crate_path: String = "rrtk::".into();
         match path {
-            path::Crate::ConstantGetter => self.constant_getter.string_path(crate_path),
-            path::Crate::NoneGetter => self.none_getter.string_path(crate_path),
+            path::Crate::ConstantGetter => {
+                reduce(crate_path, "ConstantGetter", self.constant_getter)
+            }
+            path::Crate::NoneGetter => reduce(crate_path, "NoneGetter", self.none_getter),
             path::Crate::Streams(stream_type) => self.streams.string_path(crate_path, stream_type),
-        }
-    }
-}
-struct ConstantGetter {
-    self_in_scope: bool,
-}
-impl ConstantGetter {
-    fn new() -> Self {
-        Self {
-            self_in_scope: false,
-        }
-    }
-    fn string_path(&self, super_path: String) -> String {
-        let name = "ConstantGetter";
-        if self.self_in_scope {
-            name.into()
-        } else {
-            super_path + name
-        }
-    }
-}
-struct NoneGetter {
-    self_in_scope: bool,
-}
-impl NoneGetter {
-    fn new() -> Self {
-        Self {
-            self_in_scope: false,
-        }
-    }
-    fn string_path(&self, super_path: String) -> String {
-        let name = "NoneGetter";
-        if self.self_in_scope {
-            name.into()
-        } else {
-            super_path + name
         }
     }
 }
 struct Streams {
     self_in_scope: bool,
-    expirer: streams::Expirer,
-    latest: streams::Latest,
+    expirer: bool,
+    latest: bool,
     control: streams::Control,
 }
 impl Streams {
     fn new() -> Self {
         Self {
             self_in_scope: false,
-            expirer: streams::Expirer::new(),
-            latest: streams::Latest::new(),
+            expirer: false,
+            latest: false,
             control: streams::Control::new(),
         }
     }
     fn string_path(&self, super_path: String, path: path::Streams) -> String {
-        let name = "streams::";
-        let name: String = if self.self_in_scope {
-            name.into()
-        } else {
-            super_path + name
-        };
+        let name = reduce(super_path, "streams::", self.self_in_scope);
         match path {
-            path::Streams::Expirer => self.expirer.string_path(name),
-            path::Streams::Latest => self.latest.string_path(name),
+            path::Streams::Expirer => reduce(name, "Expirer", self.expirer),
+            path::Streams::Latest => reduce(name, "Latest", self.latest),
+            path::Streams::Control(stream_type) => self.control.string_path(name, stream_type),
         }
     }
 }
 mod streams {
-    pub struct Expirer {
-        self_in_scope: bool,
-    }
-    impl Expirer {
-        pub fn new() -> Self {
-            Self {
-                self_in_scope: false,
-            }
-        }
-        pub fn string_path(&self, super_path: String) -> String {
-            let name = "Expirer";
-            if self.self_in_scope {
-                name.into()
-            } else {
-                super_path + name
-            }
-        }
-    }
-    pub struct Latest {
-        self_in_scope: bool,
-    }
-    impl Latest {
-        pub fn new() -> Self {
-            Self {
-                self_in_scope: false,
-            }
-        }
-        pub fn string_path(&self, super_path: String) -> String {
-            let name = "Latest";
-            if self.self_in_scope {
-                name.into()
-            } else {
-                super_path + name
-            }
-        }
-    }
+    use super::*;
     pub struct Control {
         self_in_scope: bool,
-        command_pid: control::CommandPID,
-        ewma_stream: control::EWMAStream,
-        moving_average_stream: control::MovingAverageStream,
-        pid_controller_stream: control::PIDControllerStream,
+        command_pid: bool,
+        ewma_stream: bool,
+        moving_average_stream: bool,
+        pid_controller_stream: bool,
     }
     impl Control {
         pub fn new() -> Self {
             Self {
                 self_in_scope: false,
-                command_pid: control::CommandPID::new(),
-                ewma_stream: control::EWMAStream::new(),
-                moving_average_stream: control::MovingAverageStream::new(),
-                pid_controller_stream: control::PIDControllerStream::new(),
+                command_pid: false,
+                ewma_stream: false,
+                moving_average_stream: false,
+                pid_controller_stream: false,
             }
         }
-    }
-    mod control {
-        pub struct CommandPID {
-            self_in_scope: bool,
-        }
-        impl CommandPID {
-            pub fn new() -> Self {
-                Self {
-                    self_in_scope: false,
+        pub fn string_path(&self, super_path: String, path: path::streams::Control) -> String {
+            let name = reduce(super_path, "control::", self.self_in_scope);
+            match path {
+                path::streams::Control::CommandPID => reduce(name, "CommandPID", self.command_pid),
+                path::streams::Control::EWMAStream => reduce(name, "EWMAStream", self.ewma_stream),
+                path::streams::Control::MovingAverageStream => {
+                    reduce(name, "MovingAverageStream", self.moving_average_stream)
                 }
-            }
-            pub fn string_path(&self, super_path: String) -> String {
-                let name = "CommandPID";
-                if self.self_in_scope {
-                    name.into()
-                } else {
-                    super_path + name
-                }
-            }
-        }
-        pub struct EWMAStream {
-            self_in_scope: bool,
-        }
-        impl EWMAStream {
-            pub fn new() -> Self {
-                Self {
-                    self_in_scope: false,
-                }
-            }
-            pub fn string_path(&self, super_path: String) -> String {
-                let name = "EWMAStream";
-                if self.self_in_scope {
-                    name.into()
-                } else {
-                    super_path + name
-                }
-            }
-        }
-        pub struct MovingAverageStream {
-            self_in_scope: bool,
-        }
-        impl MovingAverageStream {
-            pub fn new() -> Self {
-                Self {
-                    self_in_scope: false,
-                }
-            }
-            pub fn string_path(&self, super_path: String) -> String {
-                let name = "MovingAverageStream";
-                if self.self_in_scope {
-                    name.into()
-                } else {
-                    super_path + name
-                }
-            }
-        }
-        pub struct PIDControllerStream {
-            self_in_scope: bool,
-        }
-        impl PIDControllerStream {
-            pub fn new() -> Self {
-                Self {
-                    self_in_scope: false,
-                }
-            }
-            pub fn string_path(&self, super_path: String) -> String {
-                let name = "PIDControllerStream";
-                if self.self_in_scope {
-                    name.into()
-                } else {
-                    super_path + name
+                path::streams::Control::PIDControllerStream => {
+                    reduce(name, "PIDControllerStream", self.pid_controller_stream)
                 }
             }
         }
